b98ea2650e4fe9967ea7bb6f4453f449b9250305
MBeta032 <manueljosebp@gmail.com>
2026-03-11 15:36:42 -0500

    Files-FIX

--- REACT-PROJECT-HOMEWORKS/src/main.tsx
+++ REACT-PROJECT-HOMEWORKS/src/main.tsx
@@ -1,11 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import HelloWorld from './HelloWorld.tsx'
-import PrintMessage from './PrintMessage'
-import Contador from './Contador.tsx'
-import Arreglos from './Arreglos.tsx'
-import ContactoApp from "./CHALLENGE-02/ContactoApp";
-import LinkedMusicPage from './CHALLENGE-03/pages/LinkedMusicPage.tsx'
 
 
 import './index.css'
@@ -17,16 +11,7 @@ import Challenge03App from './CHALLENGE-03/Challenge03App.tsx'
 ReactDOM.createRoot(document.getElementById('root')!)
 .render(
     <React.StrictMode>
-        { /*
-        <HelloWorld />
-        <PrintMessage message='Como te va?' />
-        <PrintMessage message='Soy un mensaje!!!' />  
-        <PrintMessage suma = {4}/> 
-        <Contador suma = {3}/>
-        <Arreglos/> 
-        <ContactoApp />
-        <LinkedMusicPage />*/ }
-        <Challenge03App />
+    <Challenge03App />
     </React.StrictMode> 
 
 )
