5005483f3072f6b82dbb4d2dfada713af5f22a6a
MBeta032 <manueljosebp@gmail.com>
2026-03-11 15:40:20 -0500

    FILES-FIX

--- REACT-PROJECT-HOMEWORKS/src/main.tsx
+++ REACT-PROJECT-HOMEWORKS/src/main.tsx
@@ -1,9 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import HelloWorld from './HelloWorld.tsx'
-import PrintMessage from './PrintMessage'
-import Contador from './Contador.tsx'
-import Arreglos from './Arreglos.tsx'
 
 import './index.css'
 import ContactoApp from "./CHALLENGE-02/ContactoApp";
@@ -14,15 +10,7 @@ import ReactDOM from 'react-dom/client'
 ReactDOM.createRoot(document.getElementById('root')!)
 .render(
     <React.StrictMode>
-        { /*
-        <HelloWorld />
-        <PrintMessage message='Como te va?' />
-        <PrintMessage message='Soy un mensaje!!!' />  
-        <PrintMessage suma = {4}/> 
-        <Contador suma = {3}/>
-        <Arreglos/> */ }
-        <ContactoApp />
+    <ContactoApp/>
     </React.StrictMode> 
-
 )
 
